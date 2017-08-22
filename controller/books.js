dd7d357f5eccdb35e1ce556b64987f71e8e95ead
anggraito <anggraito.laras@gmail.com>
2017-08-22 15:47:17 +0700

    done add fitur update and delete

--- controller/books.js
+++ controller/books.js
@@ -47,10 +47,64 @@ var insertData = (req, res)=>{
   })
 }
 
-// var editData = (req, res)=>{
-//
-// }
+var findById = (req, res)=>{
+  mongodb.connect(url)
+  .then((db)=>{
+    db.collection("books").findOne({_id:ObjectId(req.params.id)})
+    .then((book)=>{
+      res.send(book)
+    })
+    .catch(err=>{
+      res.send(err)
+    })
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+}
+
+var updateData = (req, res)=>{
+  mongodb.connect(url)
+  .then((db)=>{
+    db.collection("books").findOne({_id:ObjectId(req.params.id)})
+    .then((book)=>{
+      db.collection("books").updateOne({
+        _id:ObjectId(req.params.id)
+      }, {
+        $set: {
+        title : req.body.title || book.title,
+        author : req.body.author || book.author,
+        category: req.body.category || book.category,
+        stock : req.body.stock || book.stock}
+      },{
+        upsert: true
+      })
+      .then(user => res.send(user))
+      .catch(err => res.send(err))
+    })
+    .catch(err => res.send(err))
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+}
+
+var deleteData = (req, res)=>{
+  mongodb.connect(url)
+  .then((db)=>{
+    db.collection('books').deleteOne({_id:ObjectId(req.params.id)})
+    .then(()=>{
+      res.send('Sucess delete data')
+    })
+    .catch(err=>{
+      res.send(err)
+    })
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+}
 
 module.exports={
-  getAllData, insertData
+  getAllData, insertData, findById, updateData, deleteData
 }
--- routers/books.js
+++ routers/books.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {getAllData, insertData} = require('../controller/books')
+const {getAllData, insertData, findById, updateData, deleteData} = require('../controller/books')
 
 router.get('/', getAllData)
 router.post('/', insertData)
+router.get('/:id', findById)
+router.put('/:id', updateData)
+router.delete('/:id', deleteData)
 
 module.exports = router
