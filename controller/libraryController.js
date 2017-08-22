88bca9a93263184937162cf888bbe76ad2dab859
gananggww <gananggww@gmail.com>
2017-08-22 15:02:53 +0700

    Selesai CRUD, diakses lewat model dan diakses lewat Insomnia

--- controller/libraryController.js
+++ controller/libraryController.js
@@ -1,19 +1,44 @@
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require("mongodb").ObjectId
+const Library = require("../models/library")
+const Model = new Library()
 
 const find = (req, res)=>{
-  MongoClient.connect("mongodb://localhost:27017/library", (err, db)=>{
-    if(err)throw err
-    db.collection('Book').find().toArray((err, result)=>{
-      if(err)throw  err
-      // console.log(result);
-      res.send(result)
-    })
+  Model.find(MongoClient, (err, db)=>{
+    res.send(db)
   })
 }
 
+const create = (req, res)=>{
+  Model.create(req.body, MongoClient, (err, db)=>{
+    res.send(db)
+  })
+}
 
+const edit = (req, res)=>{
+  let id = req.params.id
+  Model.edit(ObjectId,req.body, id, MongoClient, (err, db)=>{
+    res.send(db)
+  })
+}
+const remove = (req, res)=>{
+  let id = req.params.id
+  Model.remove(MongoClient, ObjectId, id, (err, db)=>{
+    res.send(db)
+  })
+}
+
+// const findOne = (req, res)=>{
+//   let title = req.params.title
+//   Model.findOne(title, MongoClient, (err, db)=>{
+//     res.send(db)
+//   })
+// }
 
 
 module.exports = {
-  find
+  find,
+  create,
+  edit,
+  remove
 }
--- router/library.js
+++ router/library.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
-
 const libraryController = require("../controller/libraryController")
 
 
-router.get('/find', libraryController.find)
+router.get("/", libraryController.find)
+router.post("/", libraryController.create)
+router.put("/:id", libraryController.edit)
+router.delete("/:id", libraryController.remove)
+
+
 
 module.exports = router
